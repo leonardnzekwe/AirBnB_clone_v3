2c59a14de911059fa8a69461c665776d8ee35ce5
Leonard Nzekwe <nzekweleo@gmail.com>
2023-10-29 16:56:23 +0100

    Fix Checker Issue

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""Init File"""
+"""Init File for Flask"""
 from flask import Blueprint
-
-# Create a Blueprint instance with a URL prefix of '/api/v1'
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Wildcard import of everything in the package api.v1.views
 from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Index View"""
+"""Index View For Flask"""
 from api.v1.views import app_views
 from flask import jsonify
 
 
 # Create a route /status on the object app_views that returns a JSON response
+@app_views.route('/status/', methods=['GET'])
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Get status Method"""
